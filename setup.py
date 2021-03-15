62e3266b98c845b01747700bdcb03d7b89d23cd6
jm12138 <2286040843@qq.com>
2021-03-16 00:42:39 +0800

    update 1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='paddleimsp',
-    version='1.0.0',
+    version='1.0.1',
     author='jm12138',
     author_email='2286040843@qq.com',
     packages=['imsp'],
